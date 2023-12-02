325c628d82ccc0e95c4fd2ab1149af13e4454379
Hamilton Ude Ikedi <hamilton.ude@decagon.dev>
2023-12-02 15:57:02 +0100

    crypto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Crypto from "./pages/Crypto";
+import Trending from "./pages/Trending";
+import Saved from "./pages/Saved";
+  
 function App() {
-
   return (
-    <>
-      <div className="bg-red-300">
-        App
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Crypto />} />
+        </Route>
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/saved" element={<Saved />} /> 
+      </Routes>
+    </BrowserRouter>
   )
 }
 
