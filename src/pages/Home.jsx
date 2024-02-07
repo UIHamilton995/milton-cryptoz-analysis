16ae2139fb95770e4b4df27617f3bca57a6215f8
Hamilton Ude Ikedi <hamilton.ude@decagon.dev>
2024-02-08 00:56:56 +0100

    logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import { Outlet, Link } from 'react-router-dom'
 import Navigation from '../components/Navigation'
+import logo from '../assets/burncoin.jpeg'
 
 const Home = () => {
   return (
     <main className='w-full h-full flex flex-col first-letter:content-center items-center relative text-white font-nunito'>
       <div className='w-screen h-screen bg-gray-300 fixed -z-10'></div>
       <Link to='/' className="font-bold text:sm sm:text-xl flex flex-wrap mr-auto ml-3">
-        <div className="w-16 h-12">
-          <img src="./src/assets/burncoin.png" alt="Crytpoz logo" className="" />  
+        <div className="w-16 h-14">
+          <img src={logo} alt="Cryptoz logo" />  
         </div>
         <div className="hidden sm:flex flex-col h-14">
           <span className="text-lg mt-1 text-cyan">Milton</span>
